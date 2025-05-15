81d9505c01f79c5fe1e3788aa6b758249f7331db
clemens_blrk <clemens@buildarocket.com>
2025-05-15 12:56:51 +0200

    add second sensor

--- serverTestMultiple.js
+++ serverTestMultiple.js
@@ -4,41 +4,56 @@ const Ant = require("gd-ant-plus");
 const socket = io("http://192.168.11.182:3000");
 
 var stick1 = new Ant.GarminStick2();
-let deviceID = 0;
+let playerMap = [
+  {
+    playerName: "Nakamura",
+    deviceID: 61433,
+    fideID: 2016192,
+    sensor: new Ant.HeartRateSensor(stick1),
+  },
+  {
+    playerName: "Caruana",
+    deviceID: 60311,
+    fideID: 2020009,
+    sensor: new Ant.HeartRateSensor(stick1),
+  },
+];
 const sensor = new Ant.HeartRateSensor(stick1);
 stick1.on("startup", function () {
   console.log("startup stick1");
   attachSensors();
   function attachSensors() {
-    // playerMap.slice(0,7).forEach((player, i) => {
-    //   player.sensor.attach(i, player.deviceID);
-    //   player.sensor.on("attached", () => {
-    //     console.log(player.playerName, "attached. device ID: ", player.deviceID);
-    //   });
-    // });
-
-    sensor.attach(0, deviceID);
-    sensor.on("attached", () => {
-      console.log("sensor attached");
+    playerMap.forEach((player, i) => {
+      player.sensor.attach(i, player.deviceID);
+      player.sensor.on("attached", () => {
+        console.log(
+          player.playerName,
+          "attached. device ID: ",
+          player.deviceID
+        );
+      });
     });
+
+    // sensor.attach(0, 0);
+    // sensor.on("attached", () => {
+    //   console.log("sensor attached");
+    // });
   }
 });
 
 addHBEventListeners();
+
 function addHBEventListeners() {
-  // playerMap.forEach((player) => {
-  //   player.sensor.on("hbData", (hbData) => {
-  //     console.log(player.playerName, hbData.DeviceID, hbData.ComputedHeartRate);
-  //     const data = {
-  //       playerData: { name: player.playerName, fideID: player.fideID },
-  //       hbData: hbData,
-  //     };
-  //     socket.emit("hbData", data);
-  //   });
-  // });
-  sensor.on("hbData", (data) => {
-    console.log("hbData", data);
-    socket.emit("hbData", data);
+  playerMap.forEach((player) => {
+    player.sensor.on("hbData", (hbData) => {
+      console.log(player.playerName, hbData.DeviceID, hbData.ComputedHeartRate);
+      const data = {
+        playerData: { name: player.playerName, fideID: player.fideID },
+        laptopID: "Clemens",
+        hbData: hbData,
+      };
+      socket.emit("hbData", data);
+    });
   });
 }
 
